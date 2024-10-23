fc00808016223a283837063378b1ff20642940f5
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-10-23 13:08:58 +0300

    update

--- terraform/argo.tf
+++ terraform/argo.tf
@@ -15,6 +15,26 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "7.6.12"
+  values = [
+    <<-EOT
+    configs:
+      applications:
+        - name: root-app
+          namespace: argocd
+          project: default
+          source:
+            repoURL: 'https://github.com/kol-ratner/dream.git'
+            targetRevision: HEAD
+            path: kubernetes/apps
+          destination:
+            server: https://kubernetes.default.svc
+            namespace: argocd
+          syncPolicy:
+            automated:
+              prune: true
+              selfHeal: true
+    EOT
+  ]
 
   depends_on = [
     kind_cluster.default,
@@ -23,31 +43,16 @@ resource "helm_release" "argocd" {
   ]
 }
 
-# resource "kubernetes_ingress" "argocd" {
-#   wait_for_load_balancer = true
-#   metadata {
-#     name      = "argocd-server"
-#     namespace = kubernetes_namespace.argocd.id
-#     annotations = {
-#       "kubernetes.io/ingress.class" = "nginx"
-#     }
-#   }
+# resource "helm_release" "argocd_image_updater" {
+#   name       = "argocd-image-updater"
+#   namespace  = kubernetes_namespace.argocd.metadata[0].name
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argocd-image-updater"
+#   # version    = "7.6.12"
 
-#   spec {
-#     rule {
-#       http {
-#         path {
-#           path = "/"
-#           backend {
-#             service_name = "argocd-server"
-#             service_port = 80
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-
-# output "load_balancer_hostname" {
-#   value = kubernetes_ingress.argocd.status.0.load_balancer.0.ingress.0.hostname
+#   depends_on = [
+#     kind_cluster.default,
+#     local_file.kubeconfig,
+#     kubernetes_namespace.argocd
+#   ]
 # }
--- terraform/nginx.tf
+++ terraform/nginx.tf
@@ -1,44 +1,44 @@
-resource "kubernetes_namespace" "nginx" {
-  metadata {
-    name = "nginx"
-  }
+# resource "kubernetes_namespace" "nginx" {
+#   metadata {
+#     name = "nginx"
+#   }
 
-  depends_on = [
-    kind_cluster.default,
-    local_file.kubeconfig
-  ]
-}
+#   depends_on = [
+#     kind_cluster.default,
+#     local_file.kubeconfig
+#   ]
+# }
 
-resource "helm_release" "ingress_nginx" {
-  name       = "ingress-nginx"
-  namespace  = kubernetes_namespace.nginx.id
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.11.3"
+# resource "helm_release" "ingress_nginx" {
+#   name       = "ingress-nginx"
+#   namespace  = kubernetes_namespace.nginx.id
+#   repository = "https://kubernetes.github.io/ingress-nginx"
+#   chart      = "ingress-nginx"
+#   version    = "4.11.3"
 
-  set {
-    name  = "namespace"
-    value = kubernetes_namespace.nginx.id
-  }
-  set {
-    name  = "controller.ingressClass"
-    value = "nginx"
-    type  = "string"
-  }
-  set {
-    name  = "controller.hostPort.enabled"
-    value = true
-    type  = "string"
-  }
-  set {
-    name  = "controller.service.type"
-    value = "NodePort"
-    type  = "string"
-  }
+#   set {
+#     name  = "namespace"
+#     value = kubernetes_namespace.nginx.id
+#   }
+#   set {
+#     name  = "controller.ingressClass"
+#     value = "nginx"
+#     type  = "string"
+#   }
+#   set {
+#     name  = "controller.hostPort.enabled"
+#     value = true
+#     type  = "string"
+#   }
+#   set {
+#     name  = "controller.service.type"
+#     value = "NodePort"
+#     type  = "string"
+#   }
 
-  depends_on = [
-    kind_cluster.default,
-    local_file.kubeconfig,
-    kubernetes_namespace.nginx
-  ]
-}
+#   depends_on = [
+#     kind_cluster.default,
+#     local_file.kubeconfig,
+#     kubernetes_namespace.nginx
+#   ]
+# }
