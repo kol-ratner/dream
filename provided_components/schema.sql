293c80ddeaa0fcca40ce983549099070360c2aa4
Kol Ratner <60704569+kol-ratner@users.noreply.github.com>
2024-10-26 22:55:22 +0300

    clean up

--- provided_components/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions
-(
-    id             SERIAL PRIMARY KEY,
-    transaction_id UUID           NOT NULL,
-    store_code     VARCHAR(255)   NOT NULL,
-    amount         DECIMAL(10, 2) NOT NULL,
-    status         VARCHAR(50)    NOT NULL,
-    processed_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
